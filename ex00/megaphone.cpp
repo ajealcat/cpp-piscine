ad5d1917df231d9ee790024ba2827c0a08e0a347
Anna JEARUTH <ajearuth@student.42.fr>
2022-06-08 16:48:27 +0200

    ex00 is done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 14:42:49 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/06/08 16:45:59 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int i;
+	int size;
+	char s;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	if (ac > 1)
+	{
+		size = 1;
+		while(size < ac)
+		{
+			i = 0;
+			while (av[size][i])
+			{
+				s = toupper(av[size][i]);
+				std::cout << s;
+				++i;
+			}
+			std::cout << ' ';
+			++size;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
