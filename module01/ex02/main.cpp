9ae7eead66e4b6054ee2f81929a1c347a09fd3a0
Anna JEARUTH <ajearuth@student.42.fr>
2022-08-18 18:13:48 +0200

    taaatayoyooooo

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -6,11 +6,19 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 12:15:03 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/08/18 12:15:15 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/08/18 18:02:18 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <iostream>
+# include <string>
+
 int main(void)
 {
+	std::string message = "HI THIS IS BRAIN";
+	std::string messagePtr = *message;
+	std::string messageRef = &message;
+
+
 	
 }
