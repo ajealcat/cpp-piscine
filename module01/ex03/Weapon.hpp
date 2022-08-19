6461ba94107e96cab88186c644ca6d0fbe6835d9
Anna JEARUTH <ajearuth@student.42.fr>
2022-08-19 15:20:43 +0200

    ex03 in progress

---
+++ module01/ex03/Weapon.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 14:01:36 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/08/19 14:04:18 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+# include <string>
+
+class Weapon
+{
+	public:
+		
+		Weapon(void);
+		~Weapon(void);
+	
+	private:
+	
+}
+
+
+#endif
