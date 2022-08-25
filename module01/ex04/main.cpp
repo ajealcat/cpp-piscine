5de5b95303b9b4cac8732f0de2b881a5ea8e9620
Anna JEARUTH <ajearuth@student.42.fr>
2022-08-25 17:11:12 +0200

    le cpp faut apprendre hein

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 10:00:54 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/08/25 12:03:25 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/08/25 16:58:02 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,25 @@
 #include <fstream>
 #include <string>
 
-int search_n_relace()
+int search_n_relace(char *to_replace, char *with_me, std::string tmp)
+{
+	std::size_t found = tmp.find(to_replace);
+	std::size_t i = 0;
+	
+	if (found == 0)
+		return(-1);
+	else
+	{
+		
+	}
+}
 
 int main(int ac, char **av)
 {
 	
 	std::ifstream	ifs;
 	std::ofstream	ofs;
-	char *tm
+	std::string 	tmp;
 
 	if (ac == 4)
 	{
@@ -34,7 +45,15 @@ int main(int ac, char **av)
 			ifs.close();
 			return (-1);
 		}
-		getline(ifs, )
+		while (getline(ifs, tmp))
+		{
+			if (ifs.badbit)
+				break ;
+			
+			if (ifs.eofbit)
+			
+		}
+		
 		ofs.close();
 		ifs.close();
 	}
