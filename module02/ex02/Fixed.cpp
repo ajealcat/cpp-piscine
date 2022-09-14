242c5beafe57f5914dbf1fb50dada49daf7cd6e5
Anna JEARUTH <ajearuth@student.42.fr>
2022-09-14 12:18:25 +0200

    lunck break

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 12:12:21 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/09/14 12:12:38 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Fixed.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Fixed::Fixed(void) : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(const int value) // prend un entier constant conv en , fixe
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = value << Fixed::_fracBits;
+}
+
+Fixed::Fixed(const float value) // prend un float conv en , fixe
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = (int)(roundf(value * (1 << Fixed::_fracBits)));
+}
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Fixed   &Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;	
+	this->_value = rhs.getRawBits();
+	return *this;
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+int	 Fixed::getRawBits(void) const
+{
+//	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+//	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+} 
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+float   Fixed::toFloat(void) const
+{
+	return (float)this->_value / (1 << Fixed::_fracBits); //  convertit la valeur en virgule fixe en nombre à virgule flottante
+}
+
+int	 Fixed::toInt(void) const
+{
+	return (int)(this->_value >> Fixed::_fracBits); // convertit la valeur en virgule fixe en nombre entier
+}
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
+{
+	o << rhs.toFloat();
+    return o;
+}
