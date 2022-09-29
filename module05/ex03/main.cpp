1faac8fa7c5b515d0c8b131dced02907768d16a2
Anna JEARUTH <ajearuth@student.42.fr>
2022-09-29 17:44:07 +0200

    je commence le module 6 il reste a finir le main de l'ex03 mais j'en ai ras le bol la pour etre polie au cas ou un recruteur tomberait sur ce commit hahaha bonjour

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:16:26 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/09/29 17:09:09 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/09/29 17:43:13 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,51 +15,70 @@
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 
 int main(void)
 {
-	ShrubberyCreationForm One("Little Tree");
-	PresidentialPardonForm Two("Silco");
-	RobotomyRequestForm Three("Ekko");
 
-	Bureaucrat a("Vi", 1);
-	Bureaucrat b("Jinx", 50);
-	Bureaucrat c("Teemo", 150);
-	std::cout << " ================================================================= " << std::endl;
 	try
-	{
-		a.signForm(One);
-		a.executeForm(One);
-		a.signForm(Two);
-		a.executeForm(Two);
-		a.signForm(Three);
-		a.executeForm(Three);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << " ================================================================= " << std::endl;
-	try
-	{
-		b.signForm(Two);
-		b.executeForm(Two);
+    {
+        Intern  stagiaire;
+        Form *newForm;
 
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}	
-	std::cout << " ================================================================= " << std::endl;
-	try
-	{
-		c.signForm(Three);
-		c.executeForm(Three);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << " ================================================================= " << std::endl;
+        newForm = stagiaire.makeForm("ShrubberyForm", "Tea Tree");
+        delete newForm;
+        newForm = stagiaire.makeForm("Bip boup", "Robot");
+        delete newForm;
+        newForm = stagiaire.makeForm("PresidentialForm", "Pardon");
+        delete newForm;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+	// ShrubberyCreationForm One("Little Tree");
+	// PresidentialPardonForm Two("Silco");
+	// RobotomyRequestForm Three("Ekko");
+
+	// Bureaucrat a("Vi", 1);
+	// Bureaucrat b("Jinx", 50);
+	// Bureaucrat c("Teemo", 150);
+	// std::cout << " ================================================================= " << std::endl;
+	// try
+	// {
+	// 	a.signForm(One);
+	// 	a.executeForm(One);
+	// 	a.signForm(Two);
+	// 	a.executeForm(Two);
+	// 	a.signForm(Three);
+	// 	a.executeForm(Three);
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
+	// std::cout << " ================================================================= " << std::endl;
+	// try
+	// {
+	// 	b.signForm(Two);
+	// 	b.executeForm(Two);
+
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }	
+	// std::cout << " ================================================================= " << std::endl;
+	// try
+	// {
+	// 	c.signForm(Three);
+	// 	c.executeForm(Three);
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
+	// std::cout << " ================================================================= " << std::endl;
 	return(0);
 }
