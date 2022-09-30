cadaea7fef1a3b5367dfe834f2a9b6748e195d56
Anna JEARUTH <ajearuth@student.42.fr>
2022-09-30 11:24:43 +0200

    ex03

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -14,7 +14,7 @@ Intern::Intern()
 	std::cout << "Default constructor Intern created." << std::endl;
 }
 
-Intern::Intern( const Intern & src )
+Intern::Intern(const Intern &src)
 {
 	*this = src;
 	std::cout << "Intern constructor copied." << std::endl;
@@ -33,7 +33,7 @@ Intern::~Intern()
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-Intern &				Intern::operator=( Intern const & rhs )
+Intern &Intern::operator=(Intern const &rhs)
 {
 	(void)rhs;
 	return *this;
@@ -43,44 +43,49 @@ Intern &				Intern::operator=( Intern const & rhs )
 ** --------------------------------- METHODS ----------------------------------
 */
 
-const char*	Intern::FormNotFoundException::what() const throw()
+const char *Intern::FormNotFoundException::what() const throw()
 {
 	return "Form not found.";
 }
 
-static	Form*	makeShrubbery(std::string const & target)
+static Form *makeShrubbery(std::string const &target)
 {
 	std::cout << "Intern creates ShrubberyCreationForm." << std::endl;
 	return new ShrubberyCreationForm(target);
 }
 
-static	Form*	makeRobotomy(std::string const & target)
+static Form *makeRobotomy(std::string const &target)
 {
 	std::cout << "Intern creates RobotomyRequestForm." << std::endl;
 	return new RobotomyRequestForm(target);
 }
 
-static	Form*	makePresidential(std::string const & target)
+static Form *makePresidential(std::string const &target)
 {
 	std::cout << "Intern creates PresidentialPardonForm." << std::endl;
 	return new PresidentialPardonForm(target);
 }
 
-Form*	Intern::makeForm(std::string const & formName, std::string const & target) const
+Form *Intern::makeForm(std::string const &formName, std::string const &target) const
 {
-	std::string	formNames[4] = {"shrubbery creation", "robotomy request", "presidential pardon", ""};
-	Form*	(*test[4])(std::string const &) = {&makeShrubbery, &makeRobotomy, &makePresidential, NULL};
-	int		i;
-
-	for (i = 0; test[i] != NULL && formNames[i] != formName; ++i);
-	if (test[i] == NULL)
+	std::string formNames[3] = {"shrubbery form", "robotomy request", "presidential pardon"};
+	Form *(*test[3])(std::string const &) = {&makeShrubbery, &makeRobotomy, &makePresidential};
+	try
+	{	
+		for (int i = 0; test[i] != NULL && formNames[i] == formName; ++i)
+			return test[i](target);
 		throw Intern::FormNotFoundException();
-	return test[i](target);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return(NULL);
+	}
+	return (NULL);
 }
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
-
 
 /* ************************************************************************** */
--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:16:26 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/09/29 17:43:13 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/09/30 11:24:07 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,60 +25,28 @@ int main(void)
         Intern  stagiaire;
         Form *newForm;
 
-        newForm = stagiaire.makeForm("ShrubberyForm", "Tea Tree");
+        newForm = stagiaire.makeForm("shrubbery form", "Tea Tree");
         delete newForm;
         newForm = stagiaire.makeForm("Bip boup", "Robot");
         delete newForm;
-        newForm = stagiaire.makeForm("PresidentialForm", "Pardon");
+        newForm = stagiaire.makeForm("presidential pardon", "Pardon");
         delete newForm;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
-    
-	// ShrubberyCreationForm One("Little Tree");
-	// PresidentialPardonForm Two("Silco");
-	// RobotomyRequestForm Three("Ekko");
-
-	// Bureaucrat a("Vi", 1);
-	// Bureaucrat b("Jinx", 50);
-	// Bureaucrat c("Teemo", 150);
-	// std::cout << " ================================================================= " << std::endl;
-	// try
+    // try
 	// {
-	// 	a.signForm(One);
-	// 	a.executeForm(One);
-	// 	a.signForm(Two);
-	// 	a.executeForm(Two);
-	// 	a.signForm(Three);
-	// 	a.executeForm(Three);
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// std::cout << " ================================================================= " << std::endl;
-	// try
-	// {
-	// 	b.signForm(Two);
-	// 	b.executeForm(Two);
-
+	// 	Intern someRandomIntern;
+	// 	Form* rrf;
+		
+	// 	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+	// 	delete rrf;
 	// }
 	// catch(const std::exception& e)
 	// {
 	// 	std::cerr << e.what() << std::endl;
 	// }	
-	// std::cout << " ================================================================= " << std::endl;
-	// try
-	// {
-	// 	c.signForm(Three);
-	// 	c.executeForm(Three);
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// std::cout << " ================================================================= " << std::endl;
 	return(0);
 }
