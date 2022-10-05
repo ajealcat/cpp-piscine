2005e34c2ac834e8730f7a4ad7102d2781308a5c
Anna JEARUTH <ajearuth@student.42.fr>
2022-10-05 14:51:30 +0200

    ex00

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 22:43:13 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/10/05 12:46:50 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/10/05 14:32:49 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,22 @@
 template<typename T>
 typename T::const_iterator	easyfind(T const & container, int const n);
 
-class	ElementNotFoundException : public std::exception {
-	const char*	what() const throw();
+class	ElementNotFoundException : public std::exception
+{
+	const char*	what() const throw()
+	{
+		return "Couldn't found the occurence.\n";
+	};
 };
 
+template<typename T>
+typename T::const_iterator	easyfind(T const & container, int const n)
+{
+	typename T::const_iterator pouet;
+	 
+	if ((pouet = find(container.begin(), container.end(), n)) == container.end())
+		throw ElementNotFoundException() ;
+	return (pouet);
+}
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,37 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 22:42:54 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/10/04 22:42:55 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/10/05 14:50:58 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::vector<int>::const_iterator i;
+	
+	std::vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(4);
+	nums.push_back(8);
+	nums.push_back(16);
+
+	try 
+	{
+		i = easyfind(nums, 8);
+		std::cout << *i << std::endl;
+	}
+	catch (std::exception &e){ std::cerr << e.what() << '\n'; }
+	try
+	{
+		i = easyfind(nums, 30);
+		std::cout << *i << std::endl;
+	}
+	catch (std::exception &e){ std::cerr << e.what() << '\n'; }
+	return(0);
+}
